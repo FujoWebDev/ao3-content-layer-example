41501d339d3f8c495339d6b03c75c5e753b94156
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-10-28 16:32:53 -0700

    allow to load multiple fics via loader

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,27 @@
 import { defineCollection } from "astro:content";
 import { getWork } from "@bobaboard/ao3.js";
 
+const removeUndefined = <T>(response: T): response is NonNullable<T> =>
+  !!response;
+
 const fanfictions = defineCollection({
   loader: async () => {
-    const response = await getWork({ workId: "38226814" });
+    const responses = await Promise.allSettled([
+      getWork({ workId: "38226814" }),
+      getWork({ workId: "49238326" }),
+      getWork({ workId: "59988091" }),
+      getWork({ workId: "41160522" }),
+    ]);
     // Must return an array of entries with an id property,
     // or an object with IDs as keys and entries as values
-    return [
-      {
-        id: "38226814",
-        ...response,
-      },
-    ];
+    return responses
+      .map((response) => {
+        if (response.status == "rejected") {
+          return;
+        }
+        return response.value;
+      })
+      .filter(removeUndefined);
   },
   // optionally add a schema
   // schema: z.object...
